a89a70e93a3f42d872a770160f9faa383c1a51d9
Dharma_KhadkaChhetri <dharmakhadkachhetri@gmail.com>
2025-10-18 15:53:27 +0545

    To Add Tasks

---
+++ 9_ToDO_App/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  return(
+    <div>
+      <h2>To Do App</h2>
+      <input value = {input} onChange={(e) => setInput(e.target.value)}></input>
+      <button onClick={() => {
+        if(input.trim() === ""){
+          return;
+        }
+        setTasks([...tasks, input]);
+        setInput("");
+        }}>Add Task</button>
+      <ul>
+        {tasks.map((task,index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default App;
